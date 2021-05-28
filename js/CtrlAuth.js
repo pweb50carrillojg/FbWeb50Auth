75261d1fa6effe7a3ea14a0595b56cf2d86bfb4b
pweb50carrillojg <84735504+pweb50carrillojg@users.noreply.github.com>
2021-05-28 08:37:38 -0500

    Create CtrlAuth.js

---
+++ js/CtrlAuth.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDdywsF6jOj4wLJgx1m4J9Z2swuuDY7gJs",
+    authDomain: "fbweb50auth.firebaseapp.com",
+    projectId: "fbweb50auth",
+    storageBucket: "fbweb50auth.appspot.com",
+    messagingSenderId: "1037425241754",
+    appId: "1:1037425241754:web:5389e0166cd882f66ef923",
+    measurementId: "G-C3WKW895NH"
+    });
